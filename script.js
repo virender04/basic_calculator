64016f5008ef56e0a39ed387ea4cdca04ef7d60e
virender04 <151429850+virender04@users.noreply.github.com>
2024-02-23 13:16:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let str = "";
+let buttons = document.querySelectorAll('.buttons');
+let output = document.querySelector('.input');
+
+
+function calculate(str) {
+    try {
+        if(str.includes('^')) {
+           str = str.replace('^', '**');
+            console.log(str);
+        }
+        const result = eval(str);
+        return result.toString();
+    } catch (error){
+        return "error";
+    }
+}
+
+Array.from(buttons).forEach(button => {
+    button.addEventListener('click', (e)=> {
+        if(e.target.innerHTML == 'C') {
+            output.innerHTML='';
+            str = '';
+            console.log(str);
+        }else if(e.target.innerHTML == '=' ) {
+            output.innerHTML= calculate(str);
+
+        }else {
+            console.log(e.target);
+            str = str + e.target.innerHTML;
+            document.querySelector('.input').innerHTML = str;
+        }
+    })
+})
